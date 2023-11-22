af8a79d9ae627535904ac0cdb16564d42862e636
Phil Dale <dalephilip@gmail.com>
2023-11-22 08:30:49 +0000

    improved tools

--- tools/tools.h
+++ tools/tools.h
@@ -14,6 +14,15 @@ std::string to_string(std::vector<std::uint8_t> const & data);
 std::string to_hex_string(std::vector<std::uint8_t> const & data);
 std::string file_to_string(std::string const& filename);
 
+template<class T> 
+T string_to(std::string &const str)
+{
+    std::stringstream convert(str);
+    T value;
+    convert >> value;
+    return value;
+}
+
 template<class T>
 std::vector<T> file_to_vec(std::string const& filename, std::string const& separator=NEW_LINE)
 {
@@ -23,10 +32,7 @@ std::vector<T> file_to_vec(std::string const& filename, std::string const& separ
     auto pos = data.find(separator, last_pos);
     while(pos != std::string::npos)
     {
-        std::stringstream convert(data.substr(last_pos, pos-last_pos));
-        T value;
-        convert >> value;
-        ret.emplace_back(value);
+        ret.emplace_back(string_to<T>(data.substr(last_pos, pos-last_pos)));
         last_pos = pos + separator.size();
         pos = data.find(separator, last_pos);
     }
