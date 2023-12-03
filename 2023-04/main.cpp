8997ad5b7839e2dd340bf80348ed8f13a7a2a579
philip-dale <78593492+philip-dale@users.noreply.github.com>
2023-12-03 11:21:41 +0000

    Prep for day 4

---
+++ 2023-04/main.cpp
@@ -0,0 +1,21 @@
+#include "../tools/tools.h"
+#include "../tools/types.h"
+
+#include <cstdint>
+#include <iostream>
+
+void part1()
+{
+}
+
+void part2()
+{
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "---- Part1 ----\n";
+    part1();
+    std::cout << "---- Part2 ----\n";
+    part2();
+}
