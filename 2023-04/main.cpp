f799f3ee5bf2a8298f7b039f419dc0d8414bc98d
philip-dale <78593492+philip-dale@users.noreply.github.com>
2023-12-04 09:20:33 +0000

    WIP

--- 2023-04/main.cpp
+++ 2023-04/main.cpp
@@ -53,7 +53,34 @@ public:
         }
         return sum;
     }
+
+    std::uint32_t win_count()
+    {
+        std::uint32_t sum{0};
+        for( auto &&n : nums){
+            for( auto &&w : wins){
+                if(n == w) {
+                    sum += 1;
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
 };
+
+void count_cards(std::vector<scrach_game> & games, std::vector<std::uint32_t> & counts, std::uint32_t id)
+{
+    counts[id] += 1;
+    for(auto i = 1; i <= games[id].win_count(); ++i)
+    {
+        if(i + id >= counts.size())
+        {
+            return;
+        }
+        count_cards(games, counts, i + id);
+    }
+}
 
 void part1()
 {
@@ -68,6 +95,19 @@ void part1()
 
 void part2()
 {
+    auto games = file_to_vec<scrach_game>("input_actual");
+    auto counts = std::vector<std::uint32_t>(games.size(), 0);
+    
+    for( auto &&g : games)
+    {
+        count_cards(games, counts, g.id-1);
+    }
+    auto sum = std::uint32_t{0};
+    for( auto &&c : counts)
+    {
+        sum += c;
+    }
+    std::cout << sum << "\n";
 }
 
 int main(int argc, char* argv[])
