3546a5855b2cf283f4068e2ffa80cecf3e0db1f0
philip-dale <78593492+philip-dale@users.noreply.github.com>
2023-12-04 14:28:42 +0000

    tidy up

--- 2023-04/main.cpp
+++ 2023-04/main.cpp
@@ -6,11 +6,23 @@
 
 class scrach_game{
 public:
-    std::uint32_t id;
-    std::vector<std::uint32_t> nums;
-    std::vector<std::uint32_t> wins;
-    std::uint32_t score;
-    std::uint32_t win_count;
+    scrach_game() :
+        m_id{0},
+        m_score{0},
+        m_win_count{0},
+        m_wins{},
+        m_nums{}
+    {};
+
+    scrach_game(std::uint32_t id, std::string & wins, std::string & nums) : 
+        m_id{id},
+        m_score{0},
+        m_win_count{0}
+    {
+        m_wins = str_to_vec<std::uint32_t>(wins, " ");
+        m_nums = str_to_vec<std::uint32_t>(nums, " ");
+        calc_score();
+    };
 
     friend std::istream &operator>> ( std::istream  &input, scrach_game &g ) {
         std::string val;
@@ -18,41 +30,58 @@ public:
 
         auto base = str_to_vec<std::string>(val, ":");
         auto id = string_to<str_num>(base[0]);
-        g.id = id.get_num();
-
         auto sides = str_to_vec<std::string>(base[1], "|");
         
-        g.wins = str_to_vec<uint32_t>(sides[0], " ");
-        g.nums = str_to_vec<uint32_t>(sides[1], " ");
-
-        g.calc_score();
+        g = scrach_game(id.get_num(), sides[0], sides[1]);
         return input;           
     };
 
+    std::uint32_t get_id()
+    {
+        return m_id;
+    };
+
+    std::uint32_t get_score()
+    {
+        return m_score;
+    };
+
+    std::uint32_t get_win_count()
+    {
+        return m_win_count;
+    };
+
+private:
     void calc_score()
     {
-        score = 0;
-        win_count = 0;
-        for( auto &&n : nums){
-            for( auto &&w : wins){
+        m_score = 0;
+        m_win_count = 0;
+        for( auto &&n : m_nums){
+            for( auto &&w : m_wins){
                 if(n == w) {
-                    if(score == 0) {
-                        score = 1;
+                    if(m_score == 0) {
+                        m_score = 1;
                     } else {
-                        score = score * 2;
+                        m_score = m_score * 2;
                     }
-                    win_count += 1;
+                    m_win_count += 1;
                     break;
                 }
             }
         }
     }
+
+    std::uint32_t m_id;
+    std::uint32_t m_score;
+    std::uint32_t m_win_count;
+    std::vector<std::uint32_t> m_nums;
+    std::vector<std::uint32_t> m_wins;
 };
 
 void count_cards(std::vector<scrach_game> & games, std::vector<std::uint32_t> & counts, std::uint32_t id)
 {
     counts[id] += 1;
-    for(auto i = 1; i <= games[id].win_count; ++i)
+    for(auto i = 1; i <= games[id].get_win_count(); ++i)
     {
         if(i + id >= counts.size())
         {
@@ -68,7 +97,7 @@ void part1()
     std::uint32_t sum = 0;
     for( auto &&g : games)
     {
-        sum += g.score;
+        sum += g.get_score();
     }
     std::cout << sum << "\n";
 }
@@ -80,7 +109,7 @@ void part2()
     
     for( auto &&g : games)
     {
-        count_cards(games, counts, g.id-1);
+        count_cards(games, counts, g.get_id()-1);
     }
     auto sum = std::uint32_t{0};
     for( auto &&c : counts)
